01c3c86ad54e4a17c8d68e0974faf06a2ff64606
Luchy-B <okparabon@gmail.com>
2023-05-20 13:46:02 +0100

    Fix linters

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -5,17 +5,19 @@ module MyEnumerable
     each { |element| return true if yield(element) }
     false
   end
+
   def any?
     return 'No block' unless block_given?
 
     each { |element| return true if yield(element) }
     false
   end
+
   def filter
     return 'No block' unless block_given?
 
     filtered_list = []
     each { |item| filtered_list << item if yield(item) }
     filtered_list
-end
+  end
 end

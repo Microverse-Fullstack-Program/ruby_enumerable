2cd117ce314da886dffda14e8cd26a8a7d51ce75
Chere Lemma <chere.lemma@aastu.edu.et>
2023-05-20 15:43:20 +0300

    Add test cases into my_list.rb

--- my_list.rb
+++ my_list.rb
@@ -12,4 +12,28 @@ class MyList
     @list.each(&block)
   end
 end
-  
+
+# Create our list
+list = MyList.new(1, 2, 3, 4)
+print(list.list)
+puts
+# <MyList: @list=[1, 2, 3, 4]>
+
+# Test #all?
+puts(list.all? { |e| e < 5 })
+# true
+
+puts(list.all? { |e| e > 5 })
+# false
+
+# Test #any?
+puts(list.any? { |e| e == 1 })
+# true
+
+puts(list.any? { |e| e == 5 })
+# false
+
+# Test #filter
+filtered_array = list.filter { |e| e&.even? }
+puts filtered_array.inspect
+# [2, 4]

042a50b4f22233ef93261721c8fa889ec6c96aec
Luchy-B <okparabon@gmail.com>
2023-05-20 13:35:02 +0100

    Add the method Any

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -5,4 +5,10 @@ module MyEnumerable
     each { |element| return true if yield(element) }
     false
   end
+  def any?
+    return 'No block' unless block_given?
+
+    each { |element| return true if yield(element) }
+    false
+  end
 end
